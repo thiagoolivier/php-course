1440d27817e290f64e1e8c7a6e668b905a39d51b
thiago oliveira <thiagoolivier53@gmail.com>
2023-01-31 13:51:42 -0400

    Added a few new folders containing new examples of use: boletophp, composer, email and  rain-tpl.

---
+++ composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'thiagooliveira/aula-de-php7',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4872f766b7dd5994eccf63136af915fb42deda03',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'thiagooliveira/aula-de-php7' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4872f766b7dd5994eccf63136af915fb42deda03',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ rain-tpl3/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4872f766b7dd5994eccf63136af915fb42deda03',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4872f766b7dd5994eccf63136af915fb42deda03',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'rain/raintpl' => array(
+            'pretty_version' => '3.1.1',
+            'version' => '3.1.1.0',
+            'reference' => 'edee683bf242f40cc75bee46a78759f6c1589dca',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rain/raintpl',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

43596cdf9883b18032fc3f30c35ed52c2fbebb77
thiago oliveira <thiagoolivier53@gmail.com>
2023-02-02 12:08:57 -0400

    Created website and seguranca folders

---
+++ website/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '124158bc548c6519ae7c7f9cb4ff49f6db68e7c3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '124158bc548c6519ae7c7f9cb4ff49f6db68e7c3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
